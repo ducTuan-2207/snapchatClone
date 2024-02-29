aa44787058e7a4ccbe1f38dd0f3aff61ec627cff
DucTuan <tuanhypc02@gmail.com>
2024-02-29 19:52:29 +0700

    Initial Commit

---
+++ snapchatClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  snapchatClone
+//
+//  Created by macOS on 29/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
